b0b6ad82271423089aaf5bb52a1e2eb378d7452b
syedazoh <160436236+syedazoh@users.noreply.github.com>
2024-12-19 10:22:14 -0800

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -27,9 +27,6 @@ bool check(const char *word)
 {
     //returns true if found else return false.
     int index = hash(word);
-    //make a cursor node which points to the head of the hash table
-    node *cursor = table[index];
-    //for loop to go through the list until the final node is reached.
     for (node *temp = cursor; temp != NULL; temp = temp->next)
     {
       if (strcasecmp(temp->word,word) == 0)
@@ -58,11 +55,6 @@ int counter = 0;
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // this function returns true if all data loaded into hash table, - false if there is an error.
-    // Stores all data in a hash table, the hash function assigns an index to each word.
-    // First, to add new data to hash table, allocate memory for nodes then add data to the node. this format copies words into the character array strcpy(n->word, "hello"); to set next pointer n->next = NULL;
-
-    // OPEN DICTIONARY FILE and read the contents.
     FILE *file = fopen(dictionary, "r");
     if (file == NULL)
     {
@@ -75,9 +67,6 @@ bool load(const char *dictionary)
     //while fscanf != EOF print the words into the character array word
     while (fscanf(file, "%s", wordlist) != EOF)
     {
-        //Keep track of how many nodes are being made
-        counter++;
-        //for each new node allocate memory.
         node *newNode = malloc(sizeof(node));
         //check for null
         if (newNode == NULL)
@@ -87,8 +76,6 @@ bool load(const char *dictionary)
         //initialize the new node by copying the word to the next node.
         strcpy(newNode->word, wordlist);
         newNode->next = NULL;
-        //add to hash table each element of the hashtable is a linked list!
-        //if index != NULL newNode->word points to the old head of the list
         int index = hash(wordlist);
         //if the index is not yet assigned, put the newest node at the head of this index.
         if (table[index] == NULL)
@@ -119,8 +106,6 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // for each of hash table 'buckets' the memory must be free'd.
-    //make a cursor which points to the head node. this cursor will work through the linked lists making each = NULL, freeing the memory.
     node *tmp = NULL;
     node *cursor = NULL;
     for (int i = 0; i < TABLE_SIZE; i++)
